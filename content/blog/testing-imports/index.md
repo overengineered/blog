2e0aaff6cb37041f05fbb57796be2f4d70f2ad0a
Juozas Kontvainis <juozask@wix.com>
2019-01-31 15:00:23 +0200

    Scaffolding

---
+++ content/blog/testing-imports/index.md
@@ -0,0 +1,6 @@
+---
+title: Some title
+date: '2019-01-31T22:12:03.284Z'
+---
+
+Placeholder text
---
+++ src/components/bio.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
+
+import { rhythm } from '../utils/typography'
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, connections } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                minWidth: 50,
+                borderRadius: `100%`,
+              }}
+              imgStyle={{
+                borderRadius: `50%`,
+              }}
+            />
+            <p>
+              Written by <strong>{author}</strong> who lives and works in
+              Vilnius. He has experience developing for many platforms,
+              currently a mobile developer for Wix.com and occasional
+              participant at {` `}
+              <a href={connections.stackOverflow}>StackOverflow</a>.
+            </p>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        connections {
+          stackOverflow
+        }
+      }
+    }
+  }
+`
+
+export default Bio
